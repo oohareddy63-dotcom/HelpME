bb3d327e7edf1a658588f3133419ac828df2ffae
oohareddy63-dotcom <oohareddy63@gmail.com>
2026-04-08 07:15:00 +0530

    Update db.js

--- services/db.js
+++ services/db.js
@@ -7,7 +7,6 @@ const connectDB = async () => {
       useUnifiedTopology: true,
       dbName: 'helpme'
     });
-
     console.log("MongoDB Connected...");
   } catch (err) {
     console.error(err.message);
