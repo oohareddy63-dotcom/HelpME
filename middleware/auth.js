c6112998cb5d0385c9df51c3f8cbbfefa760e2ae
oohareddy63-dotcom <oohareddy63@gmail.com>
2026-04-09 10:27:10 +0530

    Fix formatting in auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -12,7 +12,6 @@ module.exports = function (req, res, next) {
   }
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
     req.user = decoded.user;
     next();
   } catch (err) {
